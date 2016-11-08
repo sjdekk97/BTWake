8051ba86ac3d9b4d3901a1abb6846408166a9f36
Tim Dekker <timdeka@gmail.com>
2016-11-08 14:03:46 -0500

    Create btwake.py

---
+++ btwake.py
@@ -0,0 +1,38 @@
+from bluetooth import *
+
+
+def wake_on_lan(macaddress):
+    """ Switches on remote computers using WOL. """
+
+    # Check macaddress format and try to compensate.
+    if len(macaddress) == 12:
+        pass
+    elif len(macaddress) == 12 + 5:
+        sep = macaddress[2]
+        macaddress = macaddress.replace(sep, '')
+    else:
+        raise ValueError('Incorrect MAC address format')
+
+    # Pad the synchronization stream.
+    data = ''.join(['FFFFFFFFFFFF', macaddress * 20])
+    send_data = ''
+
+    # Split up the hex values and pack.
+    for i in range(0, len(data), 2):
+        send_data = ''.join([send_data,
+                             struct.pack('B', int(data[i: i + 2], 16))])
+
+    # Broadcast it to the LAN.
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.sendto(send_data, ('<broadcast>', 7))
+
+
+print "performing bt inquiry... for(" + sys.argv[1] + ")"
+
+nearby_devices = discover_devices(lookup_names=True)
+
+for addr, name in nearby_devices:
+    if name == sys.argv[1]:
+        wake_on_lan(addr)
+        print "Sending WOL Magic Packet to: %s - %s" % (addr, name)
